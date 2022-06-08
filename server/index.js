dd54e0eddf9b7170e53c102c78bacdacea84af6b
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-08 19:34:50 +0530

    Completed logic for registering a user

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { verify } = require("jsonwebtoken");
 const { hash, compare } = require("bcryptjs");
-
+const { fakeDB } = require("./fakeDB");
 const app = express();
 app.use(cookieParser());
 app.use(cors({
@@ -18,10 +18,20 @@ app.post("/register", async (req, res) => {
     const { email, password } = req.body;
     try {
         // Check if the user exist
-
+        const user = fakeDB.find(user => user.email === email);
+        if (user) throw new Error("User already exists");
         const hashedPassword = await hash(password, 10);
-        console.log(hashedPassword);
+        fakeDB.push({
+            id: fakeDB.length,
+            email: email,
+            password: hashedPassword
+        })
+        console.log(fakeDB);
+        res.send({ message: "User created" });
     } catch (error) {
+        res.send({
+            error: `${error.message}`;
+        })
     }
 })
 
